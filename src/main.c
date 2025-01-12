cd933d69809387601ec2a5efd61bc1e5e81dc577
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2025-01-12 19:33:43 +0100

    terminal size and frame timing

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include <termios.h>
 #include <time.h>
 #include <signal.h>
+#include <string.h>
+#include <sys/ioctl.h>
 
 #include "bad-apple.h"
 
@@ -24,6 +26,32 @@ static void reset_term() {
 
 static void singal_handler(int sig) {
     reset_term();
+}
+
+static inline int get_term_size(struct winsize *ws) {
+    memset(ws, 0, sizeof(struct winsize));
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, ws) == 0) {
+        return 0;
+    }
+
+    if (ioctl(STDIN_FILENO, TIOCGWINSZ, ws) == 0) {
+        return 0;
+    }
+
+    return ioctl(STDERR_FILENO, TIOCGWINSZ, ws);
+}
+
+static inline struct timespec timespec_sub(const struct timespec time1, const struct timespec time0) {
+    struct timespec diff = {
+        .tv_sec  = time1.tv_sec  - time0.tv_sec,
+        .tv_nsec = time1.tv_nsec - time0.tv_nsec,
+    };
+    if (diff.tv_nsec < 0) {
+        diff.tv_nsec += 1000000000; // nsec/sec
+        diff.tv_sec--;
+    }
+    return diff;
 }
 
 int main(int argc, char *argv[]) {
@@ -77,6 +105,9 @@ int main(int argc, char *argv[]) {
     struct BWImage *prev_frame = &frame1;
     struct BWImage *current_frame = &frame2;
 
+    uint32_t old_term_width = 0;
+    uint32_t old_term_height = 0;
+
     bool full_frame = true;
     double frame_duration_sec = 1.0 / bad_apple_fps;
     struct timespec frame_duration = {
@@ -85,14 +116,44 @@ int main(int argc, char *argv[]) {
     };
     frame_duration.tv_nsec = (time_t)((frame_duration_sec - (double)frame_duration.tv_sec) * 1e9);
 
+    struct timespec frame_start_ts = { .tv_sec = 0, .tv_nsec = 0 };
+    struct timespec frame_end_ts = { .tv_sec = 0, .tv_nsec = 0 };
+
     for (size_t frame_index = 0; frame_index < bad_apple_frame_count; ++ frame_index) {
+        clock_gettime(CLOCK_MONOTONIC, &frame_start_ts);
+
         const struct CompressedFrame *compr_frame = &bad_apple_frames[frame_index];
         if (!bwimage_decompress(prev_frame, compr_frame, current_frame)) {
             goto error;
         }
 
-        // TODO: center and crop image
-        printf("\x1B[1;1H");
+        struct winsize term_size;
+        uint32_t term_width;
+        uint32_t term_height;
+        uint32_t x = 0, y = 0;
+        if (get_term_size(&term_size) == 0) {
+            term_width  = (uint32_t)term_size.ws_col * 2;
+            term_height = (uint32_t)term_size.ws_row * 3;
+
+            if (term_width != old_term_width || term_height != old_term_height) {
+                full_frame = true;
+                printf("\x1B[2J");
+            }
+
+            if (bad_apple_width < term_width) {
+                x = (term_width - bad_apple_width) / 2;
+            }
+
+            if (bad_apple_height < term_height) {
+                y = (term_height - bad_apple_height) / 2;
+            }
+
+            old_term_width = term_width;
+            old_term_height = term_height;
+        }
+
+        // TODO: crop image?
+        printf("\x1B[%u;%uH", (y / 3) + 1, (x / 2) + 1);
 
         if (full_frame || true) {
             bwimage_render_ansi_full(current_frame);
@@ -108,9 +169,12 @@ int main(int argc, char *argv[]) {
         prev_frame = current_frame;
         current_frame = tmp_frame;
 
-        // TODO: measure time for more precise frame timings
-        if (nanosleep(&frame_duration, NULL) != 0) {
-            perror("nanosleep(&frame_duration, NULL)");
+        clock_gettime(CLOCK_MONOTONIC, &frame_end_ts);
+
+        struct timespec rem_duration = timespec_sub(frame_duration, timespec_sub(frame_end_ts, frame_start_ts));
+
+        if (nanosleep(&rem_duration, NULL) != 0) {
+            perror("nanosleep(&rem_duration, NULL)");
             goto error;
         }
     }
